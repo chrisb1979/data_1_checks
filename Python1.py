b65e79d01e646907573b873f97b94431b674f4a2
chrisb1979 <cboyles66@yahoo.com>
2022-09-15 14:27:56 -0700

    Python1.py

---
+++ Python1.py
@@ -0,0 +1,9 @@
+print("Hello World")
+list = ['Scream', 'Nightmare on Elm street',]
+print["Scream"]
+Characters = {'Ghostface': 'Sydney', 'Freddy': 'Nancy'}
+movie_dict = {"Ghostface": "What's your favorite scary movie", "Ghostface": "Hello Sydney"}
+print("Hello Sydney")
+Character_Tuple = ("Ghostface", "Sydney", "Freddy", "Nancy")
+
+
